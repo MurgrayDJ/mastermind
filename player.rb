df494d295e0fbf1619924b7a2241017485c4cc66
murgraydj <murgraydj@gmail.com>
2022-11-20 14:55:29 -0500

    Runs rubocop autocorrect on Player file

--- player.rb
+++ player.rb
@@ -1,13 +1,12 @@
-
 class Player
-    attr_accessor :name, :type
+  attr_accessor :name, :type
 
-    def initialize(name, type)
-        @name = name
-        @type = type
-    end
+  def initialize(name, type)
+    @name = name
+    @type = type
+  end
 end
 
-player1 = Player.new("Murgray", :codemaker)
+player1 = Player.new('Murgray', :codemaker)
 
 puts player1.type
