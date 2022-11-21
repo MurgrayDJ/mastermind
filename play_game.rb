9da18929346099561c084b948c5f9866b681ae3e
MurgrayDJ <murgraydj@gmail.com>
2022-11-20 22:06:03 -0500

    Makes rubocop suggestions on Play Game file

--- play_game.rb
+++ play_game.rb
@@ -1,25 +1,25 @@
-require './player.rb'
+require './player'
 
 class PlayGame
   def initialize
     @code_maker = createPlayer(1)
-    @code_breaker = createPlayer(2);
+    @code_breaker = createPlayer(2)
 
     puts @code_breaker.name
     puts @code_maker.name
   end
 
-  def createPlayer(player_num)
+  def create_player(player_num)
     if player_num == 1
-      player_name = "the computer"
+      player_name = 'the computer'
       player_type = :code_maker
     else
-      print "Enter player name: "
+      print 'Enter player name: '
       player_name = gets.chomp
       player_type = :code_breaker
     end
-    new_player = Player.new(player_name, player_type)
+    Player.new(player_name, player_type)
   end
 end
 
-new_game = PlayGame.new()
+PlayGame.new
