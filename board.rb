97a01d3f10aad4dcd5e6ce0c43fe1c06e3007aca
MurgrayDJ <murgraydj@gmail.com>
2022-11-26 18:04:03 -0500

    Prints entire board, allows user to make one guess

--- board.rb
+++ board.rb
@@ -19,6 +19,31 @@ class Board
     end
     puts "\n-------------------------------------------------"
   end
+
+  def print_board
+    board.each do |row_name, row_content|
+      unless row_name == :shield
+        print "----------------- "
+        print "------------------------------------------------- "
+        print "-----------------\n"
+
+        print "#{row_name[feedback1][0]}  #{row_name[feedback1][1]} || "
+        
+        row_name[guess].each do |guess_value|
+          "#{guess_value} "
+        end
+
+        print "|| "
+
+        print "#{row_name[feedback2][0]}  #{row_name[feedback2][1]}"
+
+
+        print "----------------- "
+        print "------------------------------------------------- "
+        print "-----------------\n"
+      end
+    end
+  end
 end
 
 # board = Board.new(%i[blue red green green])
--- play_game.rb
+++ play_game.rb
@@ -20,22 +20,30 @@ class PlayGame
     Player.new(player_name, player_type)
   end
 
+  def create_board
+    board = Board.new(get_shield_code)
+  end
+
   def get_shield_code
     puts "Codemaker, please enter your 4-part shield code using the following values:"
     Board.print_colors
-    shield_code = []
-
-    4.times do |spot_num|
-      prompt = "Spot #{spot_num + 1}: "
-      shield_code << get_valid_data(prompt, nil, Board.board_colors)
-    end
+    shield_code = code_entry(shield_code)
     puts "Shield code entered!"
     shield_code
   end
 
-  def create_board
-    board = Board.new(get_shield_code)
-    # puts board.board[:shield]
+  def code_entry
+    code_array = []
+    4.times do |spot_num|
+      prompt = "  Spot #{spot_num + 1}: "
+      code_array << get_valid_data(prompt, nil, Board.board_colors)
+    end
+    code_array
+  end
+
+  def get_guess(guess_num)
+    print "Codebreaker, make your guess: "
+    code_entry
   end
 
   def get_valid_data(prompt, response, valid_responses)
