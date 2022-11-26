137d458bd292eb52634bc308a440792d5c24be81
MurgrayDJ <murgraydj@gmail.com>
2022-11-26 13:43:33 -0500

    Board colors now part of board class
    They were initially located in the Play Game class.

--- board.rb
+++ board.rb
@@ -1,15 +1,25 @@
 class Board
   attr_accessor :board
+  attr_reader :board_colors
+  @@board_colors = %w(blue green yellow red orange purple)
 
   def initialize(shield)
-    @board = {
-      shield: shield
-    }
-
+    @board = {shield: shield}
     @guesses = 0
+  end
+  
+  def self.print_colors
+    @@board_colors.each do |color|
+      print "#{color} "
+    end
+    puts ":"
+  end
+
+  def set_shield
+    
   end
 end
 
-board = Board.new(%i[blue red green green])
+# board = Board.new(%i[blue red green green])
 
-print board.board[:shield]
+# print board.board[:shield]
--- play_game.rb
+++ play_game.rb
@@ -1,17 +1,16 @@
 require './player'
+require './board'
 
 class PlayGame
   def initialize
-    @code_maker = createPlayer(1)
-    @code_breaker = createPlayer(2)
-
-    puts @code_breaker.name
-    puts @code_maker.name
+    @code_maker = create_player(1)
+    @code_breaker = create_player(2)
+    @board = create_board
   end
 
   def create_player(player_num)
     if player_num == 1
-      player_name = 'the computer'
+      player_name = 'Computer'
       player_type = :code_maker
     else
       print 'Enter player name: '
@@ -19,6 +18,18 @@ class PlayGame
       player_type = :code_breaker
     end
     Player.new(player_name, player_type)
+  end
+
+  def get_shield_code
+    puts "Codemaker, please enter your shield code."
+    puts "You can enter one of the following values:"
+    Board.print_colors
+    print "Row 1:"
+    shield_code = gets.chomp
+  end
+
+  def create_board
+    board = Board.new(get_shield_code)
   end
 end
 
