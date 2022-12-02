42ebe754ebc385eef85aef6b4a88f48622a469fd
murgraydj <murgraydj@gmail.com>
2022-12-02 13:15:59 -0500

    Formats board printing to be evenly spaced

--- board.rb
+++ board.rb
@@ -24,32 +24,44 @@ class Board
   def print_board
     board.each do |row_name, row_content|
       unless row_name == :shield
-        print "----------------- "
-        print "------------------------------------------------- "
+        print "-----------------"
+        print "  -----------------------------------------------  "
         print "-----------------\n"
         print "|"
 
-        print "#{row_content["key_pegs"][0]}  #{row_content["key_pegs"][1]} || "
+        print_keypeg_or_space(row_content["key_pegs"][0])
+        print_keypeg_or_space(row_content["key_pegs"][1])
         
-        row_content["guess"].each_with_index do |guess_value, spot|
+        print "||"
+        
+        row_content["guess"].each_with_index do |code_peg, spot|
             if spot == 0
-                print "#{guess_value.capitalize} "
+                print "#{code_peg.capitalize.center(11, " ")}"
             else
-                print "| #{guess_value.capitalize} "
+                print "|#{code_peg.capitalize.center(11, " ")}"
             end
         end
 
-        print "|| "
-
-        print "#{row_content["key_pegs"][2]}  #{row_content["key_pegs"][3]}"
+        print "||"
+        
+        print_keypeg_or_space(row_content["key_pegs"][2])
+        print_keypeg_or_space(row_content["key_pegs"][3])
 
         print "|"
-        print "\n----------------- "
-        print "------------------------------------------------- "
+        print "\n-----------------"
+        print "  -----------------------------------------------  "
         print "-----------------\n"
       end
     end
     print "\n"
+  end
+
+  def print_keypeg_or_space(keypeg_spot)
+    if keypeg_spot.nil?
+      print " " * 8
+    else
+      print "#{keypeg_spot.center(8, " ")}"
+    end
   end
 end
 
